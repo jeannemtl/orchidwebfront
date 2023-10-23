971f8894cb9f629a532127c18b8b946deca7bb63
jeannemtl <shih.jeanne@gmail.com>
2023-10-23 11:43:14 -0400

    Introduction text
    text

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ export default async function Home() {
         >
           <Twitter className="h-5 w-5 text-[#1d9bf0]" />
           <p className="text-sm font-semibold text-[#1d9bf0]">
-            Introducing girlfriend bot
+            Add girlfriend bot on Telegram to hear her reply
           </p>
         </a>
         <h1
