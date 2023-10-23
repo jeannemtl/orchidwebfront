758d21aa4ca10ad4aec24a9c12c4f604abe2f963
jeannemtl <shih.jeanne@gmail.com>
2023-10-23 01:02:25 -0400

    success message and typescript added
    success message

--- components/shared/prompt.tsx
+++ components/shared/prompt.tsx
@@ -2,18 +2,23 @@
 import { useState } from "react";
 import './prompt.css';
 
+type MessageType = {
+  type: "success" | "error";
+  text: string;
+} | null;
+
 
 export default function Prompt() {
   const [isLoading, setIsLoading] = useState(false);
-
   const [inputPrompt, setInputPrompt] = useState("");
-  const [message, setMessage] = useState(""); // State for feedback to the user
+  const [message, setMessage] = useState<MessageType>(null);
 
-  const handleSubmit = async () => {
-    setIsLoading(true); 
-    const endpoint = "http://127.0.0.1:5000/send_prompt"
-; // Ensure this URL points to your backend
 
+  const endpoint = "http://127.0.0.1:5000/send_prompt"; 
+
+  const handleApiCall = async () => {
+    setIsLoading(true);
+    
     try {
       const response = await fetch(endpoint, {
         method: "POST",
@@ -26,69 +31,72 @@ export default function Prompt() {
       const data = await response.json();
 
       if (response.ok) {
-        setMessage("Prompt sent successfully!"); // Set success message
-        setInputPrompt(""); // Clear the input field
-        setIsLoading(false); 
+        setMessage({
+          type: "success",
+          text: "Check out what she says on TG!"
+        });
+        setInputPrompt(""); 
       } else {
-        setMessage(`Error: ${data.message || "Failed to send prompt"}`); // Set error message from server response or a default message
+        setMessage({
+          type: "error",
+          text: `Error: ${data.message || "Failed to send prompt"}`
+        });
       }
+      setIsLoading(false); 
     } catch (error) {
       console.error("Error sending data:", error);
-      setMessage("Failed to send prompt due to a network error."); // Set a generic error message
+      setMessage({
+        type: "error",
+        text: "Failed to send prompt due to a network error."
+      });
+      setIsLoading(false);
     }
   };
 
-  // ...
-
   return (
-    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+    <div className="prompt-container">
       <input
         type="text"
         value={inputPrompt}
         onChange={(e) => setInputPrompt(e.target.value)}
         placeholder="Go ahead, chat with her..."
-        style={{
-          width: "100%",
-          borderRadius: "15px",
-          padding: "15px 10px",
-          border: "1px solid #ccc",
-          marginBottom: "10px" // Adds space below the input field
-        }}
+        aria-label="Chat input"
+        className="prompt-input"
       />
       <button
-  onClick={handleSubmit}
-  className="group flex max-w-fit items-center justify-center space-x-2 rounded-full border border-black bg-black px-5 py-2 text-sm text-white transition-colors hover:bg-white hover:text-black"
->
-  {isLoading ? (
-    // Spinner loader here
-    <span className="spinner"></span>
-  ) : (
-    <>
-      <svg
-        className="h-4 w-4 group-hover:text-black"
-        viewBox="0 0 24 24"
-        fill="currentColor"
-        xmlns="http://www.w3.org/2000/svg"
+        onClick={handleApiCall}
+        className="submit-button"
+        aria-label="Submit"
       >
-        <path
-          d="M12 4L20 20H4L12 4Z"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-      </svg>
-      <p>Submit</p>
-    </>
-  )}
-</button>
-
+        {isLoading ? (
+          <span className="spinner" aria-label="Loading"></span>
+        ) : (
+          <>
+            <svg
+              className="submit-icon"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+            >
+              <path
+                d="M12 4L20 20H4L12 4Z"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+            <p>Submit</p>
+          </>
+        )}
+      </button>
   
-      {message && <p>{message}</p>} {/* Display feedback message */}
+      {message && (
+        <p className={`feedback-message ${message.type}`}>
+          {message.text}
+        </p>
+      )}
     </div>
   );
-  
-  
 }
-
-
