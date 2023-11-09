fe5b0487cbfe21f4e905d206f3ea96c95d7b7ee1
jeannemtl <shih.jeanne@gmail.com>
2023-11-08 21:58:51 -0500

    change in word to test
    test

--- app/page.tsx
+++ app/page.tsx
@@ -42,18 +42,14 @@ export default async function Home() {
           className="animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm [text-wrap:balance] md:text-7xl md:leading-[5rem]"
           style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
         >
-          ChatGPT with your girlfriend bot
+          ChatGPT with your girlfriend bot here
         </h1>
         <p
           className="mt-6 animate-fade-up text-center text-gray-500 opacity-0 [text-wrap:balance] md:text-xl"
           style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
-        >
-     
-        </p>
-        <Prompt/>
-        
+        ></p>
+        <Prompt />
       </div>
-      
     </>
   );
 }
